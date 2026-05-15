712b1d047eeda025196ceade6ad10bfbe50ccf3e
Adagbon <nadagbon@gmail.com>
2026-05-15 13:46:07 +0100

    Day1 notification

---
+++ day1.py
@@ -0,0 +1,7 @@
+name = "Law Adagbon"
+quote = "The expert in anything was once a beginner - Helen Hayes"
+date = "2026-05-15"
+
+print(f"My name is {name}.")
+print(f"My favorite quote is: '{quote}'")
+print(f"Today's date is: {date}")
