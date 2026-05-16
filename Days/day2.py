8a477a5606aa1115bd29a88b2a4fcfafc0f6853e
Adagbon <nadagbon@gmail.com>
2026-05-16 11:25:51 +0100

    Welcome to Day2

---
+++ Days/day2.py
@@ -0,0 +1,11 @@
+name = "Law Adagbon"
+quote = """ Learning a difficult skill takes time. But remember, the harder the skill is to learn,
+the more fulfilling it is  when you reach the finish line. ― Mark Joseph Aduana"""
+date = "2026-05-16"
+
+print(f"My name is {name}.")
+print(f"My favorite quote is: '{quote}'")
+print(f"Today's date is: {date}") 
+
+Greeting = "Welcome to Day 2 of this Journey!"
+print(Greeting)
